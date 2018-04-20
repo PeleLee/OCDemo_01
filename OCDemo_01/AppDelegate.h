e577fa1d4237246e910c9bd59a500e1e9243f51f
ThinkerLqf <804024833@qq.com>
2018-04-20 15:38:01 +0800

    Initial Commit

---
+++ OCDemo_01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCDemo_01
+//
+//  Created by liqunfei on 2018/4/20.
+//  Copyright © 2018年 My. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCDemo_01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCDemo_01
+//
+//  Created by liqunfei on 2018/4/20.
+//  Copyright © 2018年 My. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
